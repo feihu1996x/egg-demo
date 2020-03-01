cbf950b4b5c5c811f6e95e5d08f444bc0a9e3ac1
feihu1996x <gsx0312@qq.com>
2020-03-01 16:07:24 +0800

    update

--- app/controller/home.js
+++ app/controller/home.js
@@ -5,7 +5,12 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    ctx.body = 'hi, egg';
+    const res = await ctx.service.product.index();
+    // ctx.body = res;
+    await ctx.render('index.html', {
+      res,
+      list: ['a', 'b', 'c']
+    });
   }
 }
 
